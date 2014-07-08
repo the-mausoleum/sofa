5f191d69e4c3905606dbc8939f980e79a3178326
Marshall Bowers <elliott.codes@gmail.com>
2014-07-08 19:55:39 -0400

    Add watch_history table.
    Remove unneeded ‘not null’ constraint.

--- schema.sql
+++ schema.sql
@@ -2,8 +2,8 @@ drop table if exists shows;
 create table shows (
     id integer primary key autoincrement,
     public_id text not null,
-    title text not null,
-    seasons integer not null,
+    title text,
+    season_count integer,
     description text
 );
 
@@ -11,14 +11,16 @@ drop table if exists seasons;
 create table seasons (
     id integer primary key autoincrement,
     public_id integer not null,
-    episodes integer not null
+    episode_count integer
 );
 
 drop table if exists episodes;
 create table episodes (
     id integer primary key autoincrement,
     public_id integer not null,
-    title text not null
+    title text,
+    show integer,
+    season integer
 );
 
 drop table if exists users;
@@ -27,7 +29,14 @@ create table users (
     email text not null,
     username text not null,
     password text not null,
-    first_name text not null,
-    last_name text not null,
+    first_name text,
+    last_name text,
     permissions integer not null
+);
+
+drop table if exists watch_history;
+create table watch_history (
+    id integer primary key autoincrement,
+    user_id integer,
+    episode_id integer
 );
