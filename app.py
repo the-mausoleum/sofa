2acc708caf8508b223a5995c81cf754534c36805
Marshall Bowers <elliott.codes@gmail.com>
2014-07-09 17:37:43 -0400

    Switch app over to Flask-SQLAlchemy.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import re
 import sqlite3
 from flask import Flask, g, redirect, render_template, request, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import settings as SETTINGS
 
@@ -9,27 +10,48 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'db/sofa.db'),
+    SQLALCHEMY_DATABASE_URI='sqlite:///db/sofa.db',
     DEBUG=True,
     USERNAME=SETTINGS.DB_USERNAME,
     PASSWORD=SETTINGS.DB_PASSWORD
 ))
 
-def connect_db():
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
+db = SQLAlchemy(app)
 
-    return rv
+class Show(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(255), unique=True)
+    title = db.Column(db.String(255), unique=True)
+    season_count = db.Column(db.Integer)
+    description = db.Column(db.Text)
 
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
+    def __init__(self, title, season_count, description):
+        self.public_id = get_public_id(title)
+        self.title = title
+        self.season_count = season_count
+        self.description = description
 
-    return g.sqlite_db
+    def __repr__(self):
+        return '<Show %r>' % self.title
 
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    username = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    permissions = db.Column(db.Integer)
+
+    def __init__(self, email, username, password, first_name, last_name):
+        self.email = email
+        self.username = username
+        self.password = generate_password_hash(password)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.permissions = 0
+
+db.create_all()
 
 @app.route('/')
 def index():
@@ -37,37 +59,27 @@ def index():
 
 @app.route('/shows')
 def shows():
-    db = get_db()
-
-    query = db.execute('select * from shows order by title desc')
-    shows = query.fetchall()
+    shows = Show.query.all()
 
     return render_template('shows.html', shows=shows)
 
 @app.route('/shows/<show_id>')
 def show_details(show_id):
-    db = get_db()
-
-    query = db.execute('select * from shows where public_id=?', [show_id])
-    show = query.fetchone()
+    show = Show.query.filter_by(public_id=show_id).first()
 
     return render_template('shows-details.html', show=show)
 
 @app.route('/shows/add', methods=['GET', 'POST'])
 def show_add():
     if request.method == 'POST':
-        db = get_db()
-
-        show_id = get_public_id(request.form['title'])
-
-        db.execute('insert into shows (public_id, title, season_count, description) values (?, ?, ?, ?)', [
-            show_id.lower(),
+        show = Show(
             request.form['title'],
             request.form['seasons'],
             request.form['description']
-        ])
+        )
 
-        db.commit()
+        db.session.add(show)
+        db.session.commit()
 
         return redirect(url_for('shows'))
 
@@ -83,31 +95,22 @@ def show_delete(show_id):
 
 @app.route('/users')
 def users():
-    db = get_db()
-
-    query = db.execute('select * from users order by username desc')
-    users = query.fetchall()
+    users = User.query.all()
 
     return render_template('users.html', users=users)
 
 @app.route('/users/<username>')
 def user_details(username):
-    db = get_db()
-
-    query = db.execute('select * from users where username=?', [username])
-    user = query.fetchone()
+    user = User.query.filter_by(username=username).first()
 
     return render_template('user-details.html', user=user)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        db = get_db()
+        user = User.query.filter_by(username=request.form['username']).first()
 
-        query = db.execute('select * from users where username=?', [request.form['username']])
-        user = query.fetchone()
-
-        if check_password_hash(user['password'], request.form['password']):
+        if check_password_hash(user.password, request.form['password']):
             return redirect(url_for('index'))
         else:
             return 'No dice'
@@ -117,18 +120,16 @@ def login():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        db = get_db()
-
-        db.execute('insert into users (email, username, password, first_name, last_name, permissions) values (?, ?, ?, ?, ?, ?)', [
+        user = User(
             request.form['email'],
             request.form['username'],
-            generate_password_hash(request.form['password']),
+            request.form['password'],
             request.form['first_name'],
-            request.form['last_name'],
-            0
-        ])
+            request.form['last_name']
+        )
 
-        db.commit()
+        db.session.add(user)
+        db.session.commit()
 
         return redirect(url_for('index'))
 
@@ -139,7 +140,7 @@ def get_public_id(title):
     public_id = re.sub(r'[^A-Za-z0-9\-]', '', public_id)
     public_id = re.sub(r'--', '-', public_id)
 
-    return public_id
+    return public_id.lower()
 
 if __name__ == '__main__':
     app.run()
