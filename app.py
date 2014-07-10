4fb895dad765f0f5626142243333797ce7bff309
Marshall Bowers <elliott.codes@gmail.com>
2014-07-09 20:56:06 -0400

    Working show delete, with cascade.
    Had to move models back into the main file to avoid an issue with
    multiple db sessions which was preventing deletion.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
+import re
 import sqlite3
 from flask import Flask, g, redirect, render_template, request, session, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import settings as SETTINGS
-from models import *
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -19,6 +19,66 @@ app.config.update(dict(
 
 db = SQLAlchemy(app)
 
+class Show(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(255), unique=True)
+    title = db.Column(db.String(255), unique=True)
+    season_count = db.Column(db.Integer)
+    description = db.Column(db.Text)
+    episodes = db.relationship('Episode', backref='show', cascade='all, delete-orphan', lazy='dynamic')
+
+    def __init__(self, title, season_count, description):
+        self.public_id = get_public_id(title)
+        self.title = title
+        self.season_count = season_count
+        self.description = description
+
+    def __repr__(self):
+        return '<Show %r>' % self.title
+
+class Episode(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    description = db.Column(db.Text)
+    show_id = db.Column(db.Integer, db.ForeignKey('show.id'))
+
+    def __init__(self, title, description, show_id):
+        self.public_id = get_public_id(title)
+        self.title = title
+        self.description = description
+        self.show_id = show_id
+
+    def __repr__(self):
+        return '<Episode %r>' % self.title
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    username = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    permissions = db.Column(db.Integer)
+
+    def __init__(self, email, username, password, first_name, last_name):
+        self.email = email
+        self.username = username
+        self.password = generate_password_hash(password)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.permissions = 0
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+def get_public_id(title):
+    public_id = re.sub(r'\s', '-', title)
+    public_id = re.sub(r'[^A-Za-z0-9\-]', '', public_id)
+    public_id = re.sub(r'--', '-', public_id)
+
+    return public_id.lower()
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -32,7 +92,7 @@ def shows():
 @app.route('/shows/<show_id>')
 def show_details(show_id):
     show = Show.query.filter_by(public_id=show_id).first()
-    episodes = Episode.query.filter_by(show_id=show.id)
+    episodes = Episode.query.filter_by(show_id=show.id).all()
 
     return render_template('show-details.html', show=show, episodes=episodes)
 
@@ -58,7 +118,12 @@ def show_edit(show_id):
 
 @app.route('/shows/<show_id>/delete')
 def show_delete(show_id):
-    pass
+    show = Show.query.filter_by(public_id=show_id).first()
+
+    db.session.delete(show)
+    db.session.commit()
+
+    return redirect(url_for('shows'))
 
 @app.route('/shows/<show_id>/episodes')
 def episodes(show_id):
